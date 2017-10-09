d8536885b87b0b3fe3589554a75bd307419ee2a5
Brendan Le <le.brendan@gmail.com>
2017-10-09 14:48:13 -0400

    Basic movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float speed = 10;
+	public float jumpSpeed = 10;
+	
+	public LayerMask groundLayers;
+	
+	private Rigidbody2D rigidbody;
+	
+	private float jumpInterval = 0.2F;
+	private float nextJump = 0.0F;
+	private float distToGround;
+
+	private void Start()
+	{
+		rigidbody = GetComponent<Rigidbody2D>();
+		Collider2D collider = GetComponent<Collider2D>();
+		distToGround = collider.bounds.extents.y;
+		Debug.Log("Distance to ground " + distToGround.ToString());
+	}
+
+	private bool IsGrounded()
+	{
+		Vector2 top_left = new Vector2(transform.position.x - 1, transform.position.y + distToGround - 1F);
+		Vector2 bot_right = new Vector2(transform.position.x + 1, transform.position.y + distToGround + 2F);
+		return Physics2D.OverlapArea(top_left, bot_right, groundLayers);    
+	}
+	
+	
+	private void Update()
+	{
+		float moveVertical = Input.GetAxisRaw("Vertical");
+		float moveHorizontal = Input.GetAxisRaw("Horizontal");
+
+		float yVelocity = rigidbody.velocity.y;
+		if (moveVertical == 1 && IsGrounded() && Time.time > nextJump)
+		{
+			nextJump = Time.time + jumpInterval;
+			yVelocity = jumpSpeed;
+		}
+
+		rigidbody.velocity = new Vector2(moveHorizontal * speed, yVelocity);
+	}
+}

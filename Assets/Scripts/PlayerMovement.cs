ec9170036bf1eff74d01e91d3d6095d95399d25b
ceci <cecilia_berriz@brown.edu>
2017-10-14 13:39:29 -0400

    bullet in FixedUpdate in PlayerMovement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -26,8 +26,8 @@ public class PlayerMovement : MonoBehaviour {
 
 	private bool IsGrounded()
 	{
-		Vector2 top_left = new Vector2(transform.position.x - 0.1F, transform.position.y - distToGround);
-		Vector2 bot_right = new Vector2(transform.position.x + 0.1F, transform.position.y - distToGround - 0.1F);
+		Vector2 top_left = new Vector2(transform.position.x - 1, transform.position.y + distToGround - 1F);
+		Vector2 bot_right = new Vector2(transform.position.x + 1, transform.position.y + distToGround + 2F);
 		return Physics2D.OverlapArea(top_left, bot_right, groundLayers);    
 	}
 	
@@ -45,12 +45,20 @@ public class PlayerMovement : MonoBehaviour {
 		}
 
 		rigidbody.velocity = new Vector2(moveHorizontal * speed, yVelocity);
+	}
 
+	private void FixedUpdate()
+	{
 		//bullet functionality
 		if (Input.GetKeyDown (KeyCode.Space))
+
 		{
+
 			GameObject b = (GameObject)(Instantiate (bullet, transform.position + transform.up*1.5f, Quaternion.identity));
+
 			b.GetComponent<Rigidbody2D> ().AddForce (transform.up * 1000);
+			b.GetComponent<Rigidbody2D> ().AddForce (transform.right * 1000);
+
 		}
 	}
 }

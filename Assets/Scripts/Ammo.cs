ca08e86c90eaee2fbc323ac8f991ef9ae32f1fcf
Brendan Le <le.brendan@gmail.com>
2017-10-14 17:13:10 -0400

    Add ammo follow physics

--- Assets/Scripts/Ammo.cs
+++ Assets/Scripts/Ammo.cs
@@ -7,24 +7,31 @@ public class Ammo : MonoBehaviour
 	public Color Color;
 	public GameObject Player;
 
-	public float Acceleration = 0.05F;
-	private float _speed = 0;
+	public float InitialDisperseSpeed = 1F;
+	public float Acceleration = 0.01F;
+	public float DecelerationFactor = 1.2F;
+	
+	private float _speed = 0.0F;
+	private Vector3 _velocity;
 	
 
 	// Use this for initialization
 	void Start () {
-		
+		Vector2 dir = Random.insideUnitCircle.normalized;
+		Vector3 initialDirection = new Vector3(dir.x, dir.y, 0);
+		_velocity = initialDirection * InitialDisperseSpeed;
 	}
 	
 	void Update ()
 	{
-		Rigidbody2D body = this.gameObject.GetComponent<Rigidbody2D>();
+		transform.position += _velocity;
+		_velocity /= DecelerationFactor;
+			
 		Vector3 direction = Player.transform.position - transform.position;
+		transform.position += direction.normalized * _speed;
 		
-		transform.position = transform.position + direction.normalized * _speed;
 		_speed += Acceleration;
-		
-		Debug.Log(Player.transform.position);
+		Debug.Log(_speed);
 	}
 	
 	void OnTriggerEnter2D(Collider2D other)

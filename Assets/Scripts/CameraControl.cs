9133e1f671ed0cacce03fc9712f5e23579762c2a
Brendan Le <le.brendan@gmail.com>
2017-10-14 21:16:43 -0400

    Fix camera y value, extend map

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -19,7 +19,7 @@ public class CameraControl : MonoBehaviour {
 	// Update is called once per frame
 	void LateUpdate () {
 
-		transform.position = player.transform.position + offset;
+		transform.position = new Vector3(player.transform.position.x + offset.x, transform.position.y, transform.position.z);
 
 	}
 }

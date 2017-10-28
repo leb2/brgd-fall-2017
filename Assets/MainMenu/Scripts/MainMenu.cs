c6ba4531dfe8e25082d3dace4b2484e959e10157
ceci <cecilia_berriz@brown.edu>
2017-10-28 15:33:25 -0400

    main menu

--- Assets/MainMenu/Scripts/MainMenu.cs
+++ Assets/MainMenu/Scripts/MainMenu.cs
@@ -11,13 +11,16 @@ public class MainMenu : MonoBehaviour {
 		//Display our Background Texture 
 		GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height),backgroundTexture);
 
+		Time.timeScale = 0;
+
 		//buttons
 		if (GUI.Button (new Rect(Screen.width*.25f, Screen.height * .35f, Screen.width * .5f, Screen.height * .1f), "Play game")) {
-			print ("clicked!");
+			Application.LoadLevel ("Scene");
+			Time.timeScale = 1;
 		};
 
 		if (GUI.Button (new Rect(Screen.width*.25f, Screen.height * .5f, Screen.width * .5f, Screen.height * .1f), "Quit")) {
-			print ("clicked!");
+			Application.Quit();
 		};
 	}
 }

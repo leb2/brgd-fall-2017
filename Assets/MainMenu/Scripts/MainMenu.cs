12a62548923295250579bfaa8e36a6323b8f0be8
ceci <cecilia_berriz@brown.edu>
2017-11-18 15:12:14 -0500

    music

--- Assets/MainMenu/Scripts/MainMenu.cs
+++ Assets/MainMenu/Scripts/MainMenu.cs
@@ -7,7 +7,6 @@ public class MainMenu : MonoBehaviour {
 	public Texture backgroundTexture;
 
 	void OnGUI(){
-		
 		//Display our Background Texture 
 		GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height),backgroundTexture);
 
---
+++ Assets/MainMenu/Scripts/MainMusic.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMusic : MonoBehaviour {
+	
+	public AudioClip mainTheme;
+	private AudioSource source;
+
+	void Awake()
+	{
+		DontDestroyOnLoad(this.gameObject);
+		source = GetComponent<AudioSource> ();
+		source.PlayOneShot (mainTheme, 1.0f);
+	}
+
+	void Update() 
+	{
+		//source.PlayOneShot (mainTheme, 1.0f);
+	}
+}

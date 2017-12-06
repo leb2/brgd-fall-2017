0f008d273ef5ac68c03590db5606e7f716f2bb87
Brendan Le <le.brendan@gmail.com>
2017-12-06 03:08:35 -0500

    Fix issue with music overlapping itself

--- Assets/MainMenu/Scripts/MainMusic.cs
+++ Assets/MainMenu/Scripts/MainMusic.cs
@@ -6,12 +6,30 @@ public class MainMusic : MonoBehaviour {
 	
 	public AudioClip mainTheme;
 	private AudioSource source;
+//	private static bool _started = false;
+
+
+	private void Start()
+	{
+//		DontDestroyOnLoad(this.gameObject);
+		source = GetComponent<AudioSource> ();
+//		if (!_started)
+//		{
+            source.Play();//OneShot (mainTheme, 1.0f);
+//			_started = true;
+//		} 
+		
+	}
 
 	void Awake()
 	{
-		DontDestroyOnLoad(this.gameObject);
-		source = GetComponent<AudioSource> ();
-		source.Play();//OneShot (mainTheme, 1.0f);
+//		DontDestroyOnLoad(this.gameObject);
+//		source = GetComponent<AudioSource> ();
+//		if (!_started)
+//		{
+//            source.Play();//OneShot (mainTheme, 1.0f);
+//			_started = true;
+//		} 
 	}
 
 	void Update() 
